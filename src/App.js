0545ea9e849d06afb11f02b182f7716d862ad3a6
Sameer Katija <sameerkatija@gmail.com>
2024-06-13 23:35:16 +0500

    converted the class based components to function based components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
-import React from "react";
 import CardList from "./components/cardList";
 import SearchBox from "./components/searchBox";
 import "./App.css";
+import { useEffect, useState } from "react";
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      search: "",
-    };
-  }
-  componentDidMount() {
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((data) =>
-        this.setState(() => ({
-          monsters: data,
-        }))
-      );
-  }
-
-  onSearchChange = (e) => this.setState(() => ({ search: e.target.value }));
-
-  render() {
-    const { monsters, search } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonstors = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(search.toLowerCase());
-    });
-    console.log("Render from App");
-    return (
-      <div>
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox onSearchChange={onSearchChange} search={search} />
-        <CardList monsters={filteredMonstors} />
-      </div>
-    );
-  }
-}
+      .then((data) => setMonsters(data));
+  }, []);
+  const onSearchChange = (e) => setSearch(e.target.value);
+  const filteredMonstors = monsters.filter((monster) => {
+    return monster.name.toLowerCase().includes(search.toLowerCase());
+  });
+  console.log("Render from App");
+  return (
+    <div>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox onSearchChange={onSearchChange} search={search} />
+      <CardList monsters={filteredMonstors} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/searchBox/index.jsx
+++ src/components/searchBox/index.jsx
@@ -1,18 +1,17 @@
-import { Component, memo } from "react";
+import { memo } from "react";
 import "./searchBox.css";
-class SearchBox extends Component {
-  render() {
-    const { search, onSearchChange } = this.props;
-    console.log("Render from SearchList");
-    return (
-      <input
-        type="text"
-        value={search}
-        onChange={onSearchChange}
-        className="search-box monsters-search-box"
-        placeholder="Search Monstor"
-      />
-    );
-  }
-}
+
+const SearchBox = ({ search, onSearchChange }) => {
+  console.log("Render from SearchList");
+  return (
+    <input
+      type="text"
+      value={search}
+      onChange={onSearchChange}
+      className="search-box monsters-search-box"
+      placeholder="Search Monstor"
+    />
+  );
+};
+
 export default memo(SearchBox);
